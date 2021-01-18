7a86668a6b76887e6e548a77c2062c91fc44e967
Momoko Price <momokoprice@gmail.com>
2021-01-18 08:04:03 -0800

    scrape metdata and text into a df. can't get the timestamp tho

--- scripts/scraper.R
+++ scripts/scraper.R
@@ -12,23 +12,39 @@ body_nodes <- html %>%
   html_node("body") %>%
   html_children()
 
-name <- body_nodes %>%
-  xml2::xml_find_all("//div[@class='consumer-information__name']") %>% 
-  rvest::html_text() %>%
-  str_trim(side ="both")
+reviews <- body_nodes %>%
+  xml2::xml_find_all(".//div[contains(@class, 'review-card  ')]")
 
-text <- body_nodes %>%
-  xml2::xml_find_all("//p[@class='review-content__text']") %>%
-  rvest::html_text() %>%
-  str_trim(side ="both")
 
+consumer-information__review-count
+
+review_data <- tibble::tibble(
+  name = reviews %>% 
+    xml2::xml_find_first(".//div[contains(@class, 'consumer-information__name')]") %>%
+    rvest::html_text() %>%
+    str_trim(side ="both"),
+  num_reviews = reviews %>%
+    xml2::xml_find_first(".//div[contains(@class, 'consumer-information__review-count')]/span") %>%
+    rvest::html_text() %>%
+    str_trim(side ="both"),
+  rating = reviews %>%
+    xml2::xml_find_first(".//div[contains(@class, 'star-rating')]/img") %>%
+    rvest::html_attr('alt') %>%
+    str_trim(side ="both"),
+  text = reviews %>%
+    xml2::xml_find_first(".//p[contains(@class, 'review-content__text')]") %>%
+    rvest::html_text() %>%
+    str_trim(side ="both")
+)
+  
 
 
 
 # Info to scrape:
 
-# DONE name:           consumer-information__name
+# name:           consumer-information__name
 # no. of reviews: consumer-information__review-count > span
 # rating:         rating.star-rating--medium > img alt (?)
-# DONE review text:    review-content__body > p
-# timestamp:      review-content-header__dates > div > span > time
+# review text:    review-content__body > p
+# CAN'T GET THIS ONE: timestamp:      review-content-header__dates > div > span > time
+
