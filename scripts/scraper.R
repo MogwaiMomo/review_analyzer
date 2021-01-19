a467edea213be92b2af749c5efc4cf981701acf4
Momoko Price <momokoprice@gmail.com>
2021-01-19 07:20:03 -0800

    strip strings from numeric vars and change data format to numeric

--- scripts/scraper.R
+++ scripts/scraper.R
@@ -15,22 +15,26 @@ body_nodes <- html %>%
 reviews <- body_nodes %>%
   xml2::xml_find_all(".//div[contains(@class, 'review-card  ')]")
 
-
-consumer-information__review-count
-
 review_data <- tibble::tibble(
   name = reviews %>% 
     xml2::xml_find_first(".//div[contains(@class, 'consumer-information__name')]") %>%
     rvest::html_text() %>%
     str_trim(side ="both"),
+  
   num_reviews = reviews %>%
     xml2::xml_find_first(".//div[contains(@class, 'consumer-information__review-count')]/span") %>%
     rvest::html_text() %>%
-    str_trim(side ="both"),
+    str_replace_all("\\D+", "") %>%
+    str_trim(side ="both") %>%
+    as.numeric(),
+  
   rating = reviews %>%
     xml2::xml_find_first(".//div[contains(@class, 'star-rating')]/img") %>%
     rvest::html_attr('alt') %>%
-    str_trim(side ="both"),
+    str_replace_all("\\D+", "") %>%
+    str_trim(side ="both") %>%
+    as.numeric(),
+  
   text = reviews %>%
     xml2::xml_find_first(".//p[contains(@class, 'review-content__text')]") %>%
     rvest::html_text() %>%
@@ -38,6 +42,19 @@ review_data <- tibble::tibble(
 )
   
 fwrite(review_data, file="output/reviews.csv")
+review_data <- fread("output/reviews.csv")
+
+# Next steps: 
+
+# clean and format what should be number vars
+
+
+
+# cycle through a set number of pages
+
+# break if max pages exceeded
+
+# turn into function
 
 
 
