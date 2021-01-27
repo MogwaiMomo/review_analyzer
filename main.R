e40282f7a8ac6666b33f8b91a9e7ccc88c40d09c
Momoko Price <momokoprice@gmail.com>
2021-01-27 07:44:49 -0800

    start transforms to fix residual issues

--- main.R
+++ main.R
@@ -56,8 +56,8 @@ str(data)
 # Uncomment, edit & change format, if needed
 
 # data$____ <-  as.Date(data$____)
-# data$____ <-  as.factor(data$____)
-data$rating <-  as.numeric(data$rating)
+data$rating <-  as.factor(data$rating)
+# data$____ <-  as.numeric(data$____)
 # data$____ <-  as.character(review_data$____)
 
 # Uncomment, edit & flatten the data frame, if need be
@@ -117,15 +117,48 @@ plot_quants("output/plushcare_corrs.png",df)
 
 # can rating & word count predict sentiment? (use linear regression with dummy variables)
 
-# first change rating to factor
-data$rating <-  as.factor(data$rating)
-
 lm.fit <- lm(ave_sentiment ~ word_count + rating, data = sent_df)
 summary(lm.fit)
 
-par(mfrow=c(2,2)) 
-plot(lm.fit)
+# according to this model, word count & particularly low & high ratings (1, 4, 5) predict sentiment scores. Word count has a slightly negative relationship, ratings has a positive relationship.
 
+# Note that the Rsquared value is low, meaning that this model doesn't account for much of the variation in the sample. So it's not a great fit. 
+
+# Does this model fit our assumptions? Need to check our residuals ...
+run_residual_plots <- function(fit_var, output_file) {
+  png(filename=output_file,
+      width=1500,
+      height=1000,
+      units="px",
+      res=140)
+  
+  par(mfrow=c(2,2)) 
+  plot(fit_var)
+  dev.off()
+}
+  
+
+# Plot 1: Residuals vs. Fitted
+# Look for a random, even cloud. If you see curves, this indicates a non-linear relationship between predictor & outcome. Likely a transformation (start with Log) may be required. Clustering indicates a missing variable. 
+# Try  log transformation (of both the predictor and the response) to remove curve & clustering
+
+# transform just the predictor first:
+log.lm.fit <- lm(ave_sentiment ~ log(word_count) + rating, 
+                 data = sent_df)
+summary(log.lm.fit)
+run_residual_plots(log.lm.fit, "plushcare_res.png")
+
+# need to omit rows with ave_sentiment = 0 before doing a log transform on it. (How many are there?)
+
+
+# Plot 2: Normal Q-Q Plot
+# If things generally follow a straight line with higher density in the middle, that's good. you may see outliers that have high leverage. This is comparing the distribution of your residuals (which should be normal) to the residuals of a theoretical (normal sample). S-shaped curves denote extreme values/higher than normal variance, and concave/convex curves denote skewed distributions. 
+
+# Plot 3: Scale-Location Plot 
+# It’s also called Spread-Location plot. This plot shows if residuals are spread equally along the ranges of predictors. This is how you can check the assumption of equal variance (homoscedasticity). It’s good if you see a horizontal line with equally (randomly) spread points.
+
+# Plot 4: Residuals vs Leverage
+# Unlike the other plots, this time patterns are not relevant. We watch out for outlying values at the upper right corner or at the lower right corner. Those spots are the places where cases can be influential against a regression line. Look for cases outside of a dashed line, Cook’s distance. When cases are outside of the Cook’s distance (meaning they have high Cook’s distance scores), the cases are influential to the regression results. The regression results will be altered if we exclude those cases.
 
 
 
